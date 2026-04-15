0049646b4a4e51b1ba0a0b8d97d673cbfbcc7aba
Sovenable <fakhrinur16@gmail.com>
2026-04-15 20:36:21 +0700

    setup vercel deployment

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD9rdDWSnKEtvJufVsBXUBAOg5q0StXCuI",
-  authDomain: "strongfit-gym.firebaseapp.com",
-  projectId: "strongfit-gym",
-  storageBucket: "strongfit-gym.firebasestorage.app",
-  messagingSenderId: "1079863596158",
-  appId: "1:1079863596158:web:1c842d2678e48c22f5f42f"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
